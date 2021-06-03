4776429a93c4839d22a6f068f16a8dfc15c49ca7
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-03 14:16:48 -0700

    another algorithm

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,11 +5,49 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
+function isAnagram(word1, word2){//// My solution minus the replace part
+    let sort1 = word1.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('')
+    let sort2 = word2.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('')
+    return sort1 === sort2
+  }
 
 
-function isAnagram(stringA, stringB) {
-    // Code goes here
+function isAnagram(word1, word2){
+    const sanitizeString = function(str){
+      return str.toLowerCase().replace(/[^a-z\d]/g, "").split('').sort().join('')
+    }
+      return sanitizeString(word1) == sanitizeString(word2)                                 
+   }
+
+
+
+function isAnagram(stringA, stringB){/////////// THIS IS BY FAR THE FASTEST ALGORITHM
+    function createCharMap(text){
+        let charMap = {}
+
+        for (let char of text){
+            if(charMap.hasOwnProperty(char)) {
+                charMap[char]++
+            } else {
+                charMap[char] = 1
+            }
+        }
+        
+        return charMap
+    }
+
+    if (stringA.length === stringB.length) {
+        let stringAMap = createCharMap(stringA)
+        let stringBMap = createCharMap(stringB)
+        for (let char in stringAMap) {
+            if (stringAMap[char] !== stringBMap[char]) {
+                return false
+            }
+        }
+        return true
+    } else {
+        return false
+    }
 }
-
 
 module.exports = isAnagram
